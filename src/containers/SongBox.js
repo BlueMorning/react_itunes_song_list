fe0dbedc1fd499c26127a9b86bd74b9f4f214c61
Eric Carles <carles.eric@wanadoo.fr>
2018-03-14 18:54:49 +0000

    Song request configured with the limit set by the user

--- src/containers/SongBox.js
+++ src/containers/SongBox.js
@@ -6,31 +6,41 @@ class SongBox extends React.Component {
 
   constructor(props){
     super(props);
+
     this.state = {
-      url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/json",
       limit: 20,
-      songLlist: []
+      songList: []
     }
 
+    this.state.url = `https://itunes.apple.com/gb/rss/topsongs/limit=${this.state.limit}/json`,
     this.handleRequestGetSongList = this.handleRequestGetSongList.bind(this);
+
+
+    this.handleLimitChange = this.handleLimitChange.bind(this);
   }
 
 
   render(){
     return (<div className="flex-box-row">
-              <input type="number" id="song-count-limit" placeholder="Set the limit of songs"/>
-              <button onClick="handleRequestGetSongList">GET SONGS</button>
+              <input type="number" id="song-count-limit" value={this.state.limit} onChange={this.handleLimitChange} placeholder="Set the limit of songs"/>
+              <button onClick={this.handleRequestGetSongList}>GET SONGS</button>
             </div>
           );
 
   }
 
+  handleLimitChange(event){
+    this.setState({limit: event.target.value});
+  }
+
   handleRequestGetSongList(){
+    this.state.url = `https://itunes.apple.com/gb/rss/topsongs/limit=${this.state.limit}/json`;
     const request = new XMLHttpRequest();
     request.open("GET", this.state.url);
     request.addEventListener("load", () => {
-      let jsonSongList = JSON.parse(this.request.responseText);
-      this.setState({songList: jsonSongList.feed.entry})
+      let jsonSongList = JSON.parse(request.responseText);
+      this.setState({songList: jsonSongList.feed.entry});
+      console.log(this.state.url);
     })
     request.send();
   }
